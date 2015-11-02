5962bc5562ddb51300ef9612bf37fc2413cda91b
Quentin Dorleat <quentin.dorleat@cern.ch>
2015-11-02 14:39:08 +0000

    delete all before add modifications from branch

--- build/copy_test_files_for_UnitTests.sh
+++
@@ -1,4 +0,0 @@
-# copy test files
-cp -R ../source/tests/Calc				./tests/test_files 
-cp -R ../source/tests/SimulationTests	./tests/test_files 
-cp -R ../source/tests/MATHISCURRENT		./tests/test_files 
--- source/Measurements/TEDM.cpp
+++
@@ -1,9 +0,0 @@
-#include "TEDM.h"
-#include "TLGCObsSummary.h"
-
-TLGCObsSummary  TEDM::getDSPTObsSummary() const{
-	TLGCObsSummary summary;
-	for(auto const& ItEDM: measDSPT)
-		summary.addNewResidual(ItEDM.getDistanceResidual()*LGC::M2MM);
-	return summary;	
-}
