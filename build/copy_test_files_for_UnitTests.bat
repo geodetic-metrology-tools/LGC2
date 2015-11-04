fe816db57b77815ebdc09e3a2575a87da00fe72f
Quentin Dorleat <quentin.dorleat@cern.ch>
2015-11-04 13:16:41 +0000

    redirect ouput on batch files > NUL (doesn't display messages when calling cmake) Harmonization work with CMakeLists.txt files

--- build/copy_test_files_for_UnitTests.bat
+++ build/copy_test_files_for_UnitTests.bat
@@ -2,7 +2,7 @@
 
 FOR %%A IN (Release Debug) DO (
 	REM copy test files
-	xcopy ..\source\tests\Calc 		   		.\tests\test_files\Calc /S /I /R /Y
-	xcopy ..\source\tests\SimulationTests   .\tests\test_files\SimulationTests /S /I /R /Y
-	xcopy ..\source\tests\MATHISCURRENT     .\tests\test_files\MATHISCURRENT /S /I /R /Y
+	xcopy ..\source\tests\Calc 		   		.\tests\test_files\Calc  			>NUL /S /I /R /Y
+	xcopy ..\source\tests\SimulationTests   .\tests\test_files\SimulationTests  >NUL /S /I /R /Y
+	xcopy ..\source\tests\MATHISCURRENT     .\tests\test_files\MATHISCURRENT 	>NUL /S /I /R /Y
 )
