39af6cec99f90796f468a04d91010c29a1443e72
Marie Barbier <marie.barbier@cern.ch>
2016-09-15 15:23:04 +0000

    merge: work with surveylib 3.05

--- build/copy_test_files_for_UnitTests.bat
+++ build/copy_test_files_for_UnitTests.bat
@@ -2,7 +2,7 @@
 
 FOR %%A IN (Release Debug) DO (
 	REM copy test files
-	xcopy ..\source\tests\Calc 		   		.\tests\test_files\Calc  			>NUL /S /I /R /Y
-	xcopy ..\source\tests\SimulationTests   .\tests\test_files\SimulationTests  >NUL /S /I /R /Y
-	xcopy ..\source\tests\MATHISCURRENT     .\tests\test_files\MATHISCURRENT 	>NUL /S /I /R /Y
+	xcopy ..\source\LGC_Tests\testFiles\Calc 		   		.\LGC_Tests\test_files\Calc  			>NUL /S /I /R /Y
+	xcopy ..\source\LGC_Tests\testFiles\SimulationTests   .\LGC_Tests\test_files\SimulationTests  >NUL /S /I /R /Y
+	xcopy ..\source\LGC_Tests\testFiles\MATHISCURRENT     .\LGC_Tests\test_files\MATHISCURRENT 	>NUL /S /I /R /Y
 )
--- build/copy_test_files_for_UnitTests.sh
+++ build/copy_test_files_for_UnitTests.sh
@@ -1,4 +1,4 @@
 # copy test files
-cp -R ../source/tests/Calc				./tests/test_files 
-cp -R ../source/tests/SimulationTests	./tests/test_files 
-cp -R ../source/tests/MATHISCURRENT		./tests/test_files 
+cp -R ../source/LGC_Tests/testFiles/Calc				./LGC_Tests/test_files 
+cp -R ../source/LGC_Tests/testFiles/SimulationTests	./LGC_Tests/test_files 
+cp -R ../source/LGC_Tests/testFiles/MATHISCURRENT		./LGC_Tests/test_files 
