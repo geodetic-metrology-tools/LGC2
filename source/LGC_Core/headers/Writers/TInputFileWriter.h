65b318ced5e2ccbc359b3a45ae48681af790dcfd
Carita Logren <carita.logren@cern.ch>
2017-02-08 10:35:05 +0100

    Create a general input file writer.

---
+++ source/LGC_Core/headers/Writers/TInputFileWriter.h
@@ -0,0 +1,54 @@
+#ifndef SU_INPUT_FILE_WRITER
+#define SU_INPUT_FILE_WRITER
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+//Copyright 2017 CERN EN/ACE/SU.  All rights reserved.
+
+//LGC
+#include <TSimFileWriter.h>
+
+class	TLGCProject;
+
+
+/*!
+\ingroup Writers
+\brief Write an LGC input file from the virtual project data.
+*/
+class  TInputFileWriter : public TSimFileWriter  
+{
+public:
+
+	/*!@name Constructors and Destructors*/
+		//@{
+			/// constructor
+			TInputFileWriter(TAStreamFormatter* stream, const TLGCData* project);
+
+			/// Destructor
+			virtual  ~TInputFileWriter();
+		//@}
+
+
+	/*!@name Public member functions*/
+		//@{
+			/// write the lgc file used when there's no error in the project
+			virtual void	writeFile();
+
+			/// write the lgc file  when there is an error in the project
+			virtual void	writeFile(const std::string error);
+		//@}
+
+protected:
+
+	/*!default constructor*/
+	TInputFileWriter();
+
+	/// write the title, the referential and the options
+	virtual void writeHeader();
+
+};
+
+/*@}*/
+
+#endif // SU_INPUT_FILE_WRITER
---
+++ source/LGC_Core/sources/Writers/TInputFileWriter.cpp
@@ -0,0 +1,135 @@
+////////////////////////////////////////////////////////////////////
+// TInputFileWriter.cpp : implementation class
+// Write an LGC "input" file from the current virtual LGC data
+//
+// Copyright 2003-2008 M.Jones, CERN, EST/SU. All rights reserved.
+////////////////////////////////////////////////////////////////////////////////////
+
+#include "TInputFileWriter.h"
+
+#include "TSeparatedFormatTStream.h"
+#include "TLGCData.h"
+
+//////////////////////////////////////////////////////////////////////
+// Definitions and Initialisations
+//////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
+//constructor / destructor
+/////////////////////////////////////////////////////////////////////////////
+
+TInputFileWriter::TInputFileWriter() : TSimFileWriter(){}
+
+
+TInputFileWriter::TInputFileWriter(TAStreamFormatter* stream, const TLGCData* project)
+    : TSimFileWriter(stream, project){}
+
+
+TInputFileWriter::~TInputFileWriter(){}
+
+
+/////////////////////////////
+// public member functions
+/////////////////////////////
+
+void TInputFileWriter::writeFile(){
+    TSimFileWriter::writeFile();
+}
+
+void TInputFileWriter::writeFile(const std::string error){
+    //write error messages from project
+    TSimFileWriter::writeFile(error);
+}
+
+/////////////////////////////
+// protected member functions
+/////////////////////////////
+
+void TInputFileWriter::writeHeader()
+{
+	TAStreamFormatter* stream = getStream();
+    const auto &config = data->getConfig();
+	
+	(*stream) << "*TITR" << endl;
+	(*stream) << config.title << endl;
+
+    // Ref system:
+    if(config.referential == TRefSystemFactory::kCernXYHg85Machine)
+		(*stream) << "*LEP" << endl;
+    else if(config.referential == TRefSystemFactory::kCERNXYHsSphereSPS)
+		(*stream) << "*SPHE" << endl;
+    else if(config.referential == TRefSystemFactory::kCernXYHg00Machine)
+		(*stream) << "*RS2K" << endl;
+	else
+		(*stream) << "*OLOC" << endl;
+
+    // Calculation options:
+	if (config.allfixed.isActive())
+		(*stream) << "*ALLFIXED" << endl;
+	else if (config.libre.isActive())
+		(*stream) << "*LIBR" << endl;
+
+	if (config.sim.isActive())
+		(*stream) << "*SIMU " << config.sim.numSims<<endl;
+
+    // Output options:
+    if(config.useApriori.isActive())
+        (*stream) << "*APRI" << endl;
+
+    if(config.covar.isActive())
+        (*stream) << "*COVAR" << endl;
+
+    if(config.writeDefa.isActive())
+        (*stream) << "*DEFA" << endl;
+
+    if(!config.erelPairs.empty()){
+        (*stream) << "*EREL" << endl;
+        for(const auto &erelpair : config.erelPairs) {
+            (*stream) << erelpair.first << "  " << erelpair.second << endl;
+        }
+    }
+
+    if(config.faut.isActive())
+        (*stream) << "*FAUT " << config.faut.alpha << "  " << config.faut.beta << endl;
+
+    if(config.CustomOutputSeparatorPunch.isActive())
+        (*stream) << "*FMTP SEP \"" << config.CustomOutputSeparatorPunch.separator << "\"" << endl;
+
+	if (config.histo.isActive())
+		(*stream) << "*HIST" << endl;
+
+	if (config.nodup.isActive())
+		(*stream) << "*NODUP" << endl;
+
+	if (config.outPrecision.digits != 5 )
+		(*stream) << "*PREC " << config.outPrecision.digits <<  endl;
+
+	if (config.errorEllipses.isActive())
+		(*stream) << "*PRES" << endl;
+
+    if(config.writePunch.isActive()){
+        if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kE)
+            (*stream) << "*PUNC E" << endl;
+        else if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kEE)
+            (*stream) << "*PUNC EE" << endl;
+        else if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kH)
+            (*stream) << "*PUNC H" << endl;
+        else if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kHN)
+            (*stream) << "*PUNC HN" << endl;
+        else if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kHZ)
+            (*stream) << "*PUNC HZ" << endl;
+        else if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kZ)
+            (*stream) << "*PUNC Z" << endl;
+        else if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kZHN)
+            (*stream) << "*PUNC ZHN" << endl;
+        else if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kT)
+            (*stream) << "*PUNC T" << endl;
+        else if(config.writePunch.fmode == TLGCConfig::TCoordOut::eMode::kOUT1)
+            (*stream) << "*PUNC OUT1" << endl;
+        else
+            (*stream) << "*PUNC" << endl;
+    }
+    
+    if(config.sim.writeLGCFile)
+        (*stream) << "*SOBS" << endl;
+}
