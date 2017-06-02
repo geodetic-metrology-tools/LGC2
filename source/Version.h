e6f9a6bc6f00e913693ee0e9fdda20ff59410cb3
mbarbier <marie.barbier@cern.ch>
2017-06-02 15:34:57 +0200

    version number in bracket fpr base 8

--- source/Version.h
+++ source/Version.h
@@ -9,7 +9,7 @@
 
 #define LGC_VERSION_MAJOR 2
 #define LGC_VERSION_MINOR 00
-#define LGC_VERSION_PATCH 08
+#define LGC_VERSION_PATCH '08'
 
 static std::string getLGCVersion(void) {
 	stringstream ss;
