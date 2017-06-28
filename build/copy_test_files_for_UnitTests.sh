70bf15ebda387da0cdd691f65518fb8eaee43825
Carita Logren <carita.logren@cern.ch>
2017-06-28 11:37:08 +0200

    Remove the now useless test file copying scripts.

--- build/copy_test_files_for_UnitTests.sh
+++
@@ -1,4 +0,0 @@
-# copy test files
-cp -R ../source/LGC_Tests/testFiles/Calc				./LGC_Tests/test_files 
-cp -R ../source/LGC_Tests/testFiles/SimulationTests	./LGC_Tests/test_files 
-cp -R ../source/LGC_Tests/testFiles/MATHISCURRENT		./LGC_Tests/test_files 
