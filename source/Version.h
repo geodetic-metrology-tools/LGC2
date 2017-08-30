5af41b306c2e5b5a9f6dccf442900d75192c738c
mbarbier <marie.barbier@cern.ch>
2017-08-30 13:31:51 +0200

    new version 2.01.01 integrating activated/desactivated option for the ui

--- source/Version.h
+++ source/Version.h
@@ -9,7 +9,7 @@
 
 #define LGC_VERSION_MAJOR 2
 #define LGC_VERSION_MINOR 01
-#define LGC_VERSION_PATCH 00
+#define LGC_VERSION_PATCH 01
 
 static std::string getLGCVersion(void) {
 	stringstream ss;
