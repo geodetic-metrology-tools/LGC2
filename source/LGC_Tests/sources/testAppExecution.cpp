d9a2847ec1dc63629cb189575d7d996d2f1ef8db
mbarbier <marie.barbier@cern.ch>
2018-02-26 10:48:39 +0100

    change default test file (use old one to have correct CI test)

--- source/LGC_Tests/sources/testAppExecution.cpp
+++ source/LGC_Tests/sources/testAppExecution.cpp
@@ -43,8 +43,8 @@ namespace tut
 		using namespace LGC;
 		set_test_name("Testing TLGCApp class and nam file reader");
 
-		TLGCApp proj(getCurrentDirectory() + slash + "test_files" + slash + "Calc" + slash + "chaba_SPHE.lgc",
-			getCurrentDirectory() + slash + "test_files" + slash + "Calc" + slash + "chaba_SPHE.res");
+		TLGCApp proj(getCurrentDirectory() + slash + "test_files" + slash + "Calc" + slash + "OBSXYZ.lgc",
+			getCurrentDirectory() + slash + "test_files" + slash + "Calc" + slash + "OBSXYZ.res");
 
 		ensure_equals("Calculation successful", proj.exec().code(), Behavior::BehaviorCode::ERR_noError);
 
