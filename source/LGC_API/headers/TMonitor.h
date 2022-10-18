0ad5ebcced4a6c5d05f90d6b17859444c08e6831
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2022-10-18 17:46:55 +0200

    working on monitoring

---
+++ source/LGC_API/headers/TMonitor.h
@@ -0,0 +1,16 @@
+#include <iostream>
+//STL
+#include <random>
+#include <TLGCData.h>
+
+
+using namespace std;
+class TMonitor
+{
+public:
+    void TMonitor::BasicObject();
+    // for FRAS stress test
+    void TMonitor::manipulate_ECWS_measurements(TLGCData *data);
+	/// Random numbers generator
+	std::ranlux48 engine;
+};
