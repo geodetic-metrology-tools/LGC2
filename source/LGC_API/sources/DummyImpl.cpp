585a06f344ded9a5fa4c821ab9adb38de4fbd89f
Jürgen Gutekunst <Juergen.gutekunst@posteo.de>
2022-12-02 16:55:28 +0100

    learning pimpl

--- source/LGC_API/sources/DummyImpl.cpp
+++ source/LGC_API/sources/DummyImpl.cpp
@@ -5,25 +5,20 @@
 
 
 // constructor
-DummyImpl::DummyImpl() : result(0), number(0)
-{
-	std::cout << "Hello"<< std ::endl;
-}
-void DummyImpl::setNumber(int k)
+void Dummy::DummyImpl::setNumber(int k)
 {
 	number = k;
 }
-void DummyImpl::printResult()
+void Dummy::DummyImpl::printResult()
 {
 	compute();
 	std::cout << "square of " << number<< "="<< result << std::endl;
 }
-void DummyImpl::compute()
+void Dummy::DummyImpl::compute()
 {
 	result = number*number;
 }
 
-/*
 void Dummy::setNumber(int k)
 {
 	impl_->setNumber(k);
@@ -33,6 +28,5 @@ void Dummy::printResult()
 	impl_->printResult();
 }
 
-Dummy::Dummy() : impl_(new DummyImpl){}
+Dummy::Dummy() : impl_(new DummyImpl()){}
 Dummy::~Dummy() = default;
-*/
