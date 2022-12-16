51935522b2d1eab562f5713770dfac188f3873cd
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2022-12-16 16:15:27 +0100

    missing dimension initialization

--- source/LGC_Core/headers/LGCProjectData/TLGCData.h
+++ source/LGC_Core/headers/LGCProjectData/TLGCData.h
@@ -229,11 +229,11 @@ public:
         bool isLGCv1() const { return islgc1; }
 
         void setLGCv1(bool set) { islgc1 = set; }
-		// frames that end with SLAVE. Their defining helmert parameters shall be constrained to be equal across all SLAVE frames
+		// frames that start with prefix SLAVE. Their defining helmert parameters shall be constrained to be equal across all SLAVE frames
 		struct 
 		{
-			int numberSlaveFrames;
-			int dimSlave;
+			int numberSlaveFrames = 0;
+			int dimSlave = 0;
 		} slaveFrames;
 
 	private:
