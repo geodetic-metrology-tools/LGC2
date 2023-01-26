a4bfbe83dce1a711e332cf92c93fe236da39bb41
Jürgen <juergen.gutekunst@iwr.uni-heidelberg.de>
2023-01-26 21:15:14 +0100

    Linux specific flags

--- source/LGC_API/headers/Moni.h
+++ source/LGC_API/headers/Moni.h
@@ -7,28 +7,36 @@
 #include <string>
 #include <memory>
 
+#ifndef __linux__
+
+#define DECLSPEC __declspec(dllexport)
+	#define DLLAPI __cdecl
+#else
+	#define DECLSPEC
+#endif
+
 class Moni
 {
 public:
-	__declspec(dllexport) Moni(std::string inputFilePath);
-	__declspec(dllexport) ~Moni();
-	__declspec(dllexport) void updateMeas(std::string id, Eigen::VectorXd measurementVector);
+	DECLSPEC Moni(std::string inputFilePath);
+	DECLSPEC ~Moni();
+	DECLSPEC void updateMeas(std::string id, Eigen::VectorXd measurementVector);
 	// triggering the adjustment claculation
-	__declspec(dllexport) void adjust();
+	DECLSPEC void adjust();
 	// get estimate of parameter
-	__declspec(dllexport) Eigen::VectorXd getEstimate(std::string);
+	DECLSPEC Eigen::VectorXd getEstimate(std::string);
 	// get estimate of parameter in subframe
-	__declspec(dllexport) Eigen::VectorXd getEstimate(std::string, std::string);
+	DECLSPEC Eigen::VectorXd getEstimate(std::string, std::string);
 	// get diagonal elements of covariances of the estimated parameters
-	__declspec(dllexport) Eigen::VectorXd getEstimateCovar(std::string);
+	DECLSPEC Eigen::VectorXd getEstimateCovar(std::string);
 	// get diagonal elements of covariances of the estimated parameters in a subframe, in a first version only Root frame is allowed
-	__declspec(dllexport) Eigen::VectorXd getEstimateCovar(std::string pointName, std::string frameName);
+	DECLSPEC Eigen::VectorXd getEstimateCovar(std::string pointName, std::string frameName);
 	// get Meas IDs
-	__declspec(dllexport) std::vector<std::string> getECWSMeasIds();
+	DECLSPEC std::vector<std::string> getECWSMeasIds();
 	// get measurement
-	__declspec(dllexport) Eigen::VectorXd getMeas(std::string id);
+	DECLSPEC Eigen::VectorXd getMeas(std::string id);
 	// get the sigma0 after adjustment
-	__declspec(dllexport) double getSigma0();
+	DECLSPEC double getSigma0();
 	
 private:
 	class MoniImpl;
