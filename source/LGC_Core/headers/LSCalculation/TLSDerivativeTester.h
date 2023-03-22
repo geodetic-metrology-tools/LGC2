7b90beddb1707eb353c4d0985b331efee17239fe
Jürgen Gutekunst <Juergen.gutekunst@posteo.de>
2023-03-22 12:50:23 +0100

    working on getParameter method for evaluator

---
+++ source/LGC_Core/headers/LSCalculation/TLSDerivativeTester.h
@@ -0,0 +1,30 @@
+/*
+© Copyright CERN 2000-2023. All rigths reserved. This software is released under a CERN proprietary software licence.
+Any permission to use it shall be granted in writing. Request shall be adressed to CERN through mail-KT@cern.ch
+*/
+
+#ifndef TLS_DERIVATIVETESTER
+#define TLS_DERIVATIVETESTER
+
+
+//LGC
+#include <TLSEvaluator.h>
+
+#include <Eigen/Dense>
+
+
+/*!
+\ingroup DerivativeTester
+\brief For testing the derivatives (A matrix) using finite differences applied to misclosure evaluations
+*/
+class TLSDerivativeTester 
+{
+
+public:
+	TLSDerivativeTester(std::shared_ptr<TLGCData> data);
+
+private:
+
+};
+
+#endif
