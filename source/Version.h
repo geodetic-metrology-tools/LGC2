63def442c3ecfe80b9bd7280ffc5ac8d2f60a2b0
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2023-03-24 11:48:18 +0100

    beta version number

--- source/Version.h
+++ source/Version.h
@@ -8,8 +8,8 @@
 #include <sstream>
 
 #define LGC_VERSION_MAJOR "2"
-#define LGC_VERSION_MINOR "5"
-#define LGC_VERSION_PATCH "1"
+#define LGC_VERSION_MINOR "6"
+#define LGC_VERSION_PATCH "beta_1_CONSI_check_update"
 
 static std::string getLGCVersion(void) {
 	std::stringstream ss;
