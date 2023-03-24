12ab34154f0334c5629472d09f35831e8a13bfda
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2023-03-24 12:11:04 +0100

    version number for experimental version

--- source/Version.h
+++ source/Version.h
@@ -8,8 +8,8 @@
 #include <sstream>
 
 #define LGC_VERSION_MAJOR "2"
-#define LGC_VERSION_MINOR "5"
-#define LGC_VERSION_PATCH "1"
+#define LGC_VERSION_MINOR "6"
+#define LGC_VERSION_PATCH "beta_2-SLAVE_Frames"
 
 static std::string getLGCVersion(void) {
 	std::stringstream ss;
