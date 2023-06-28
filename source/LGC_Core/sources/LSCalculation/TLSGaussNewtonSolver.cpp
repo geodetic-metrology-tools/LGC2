ed6da2075cef20d25c97c47cb325abcd6050b5d5
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2023-06-28 09:31:47 +0200

    extra Levenberg Marquardt solver class

--- source/LGC_Core/sources/LSCalculation/TLSGaussNewtonSolver.cpp
+++ source/LGC_Core/sources/LSCalculation/TLSGaussNewtonSolver.cpp
@@ -3,66 +3,9 @@
 #include <Logger.hpp>
 #include <iostream>
 
-#include <unsupported/Eigen/NonLinearOptimization>
-#include <unsupported/Eigen/NumericalDiff>
-
-// Generic functor
-template<typename _Scalar, int NX = Eigen::Dynamic, int NY = Eigen::Dynamic>
-struct Functor
-{
-	typedef _Scalar Scalar;
-	enum
-	{
-		InputsAtCompileTime = NX,
-		ValuesAtCompileTime = NY
-	};
-	typedef Eigen::Matrix<Scalar, InputsAtCompileTime, 1> InputType;
-	typedef Eigen::Matrix<Scalar, ValuesAtCompileTime, 1> ValueType;
-	typedef Eigen::Matrix<Scalar, ValuesAtCompileTime, InputsAtCompileTime> JacobianType;
-
-	int m_inputs, m_values;
-
-	Functor() : m_inputs(InputsAtCompileTime), m_values(ValuesAtCompileTime) {}
-	Functor(int inputs, int values) : m_inputs(inputs), m_values(values) {}
-
-	int inputs() const { return m_inputs; }
-	int values() const { return m_values; }
-};
-
-struct my_functor : Functor<double>
-{
-	my_functor(void) : Functor<double>(1, 1) {}
-	int operator()(const Eigen::VectorXd &x, Eigen::VectorXd &fvec) const
-	{
-		// Implement y = 10*(x0+3)^2 + (x1-5)^2
-		fvec(0) = acos(x(0) / pow(1 + x(0) * x(0), 0.5)) - PI / 4;
-		//fvec(0) = 10.0 * pow(x(0) + 3.0, 2) + pow(x(1) - 5.0, 2);
-		//fvec(1) = 0;
-
-		return 0;
-	}
-};
-
 
 TLSGaussNewtonSolver::TLSGaussNewtonSolver(std::shared_ptr<TLGCData> data) : fEvaluator( TLSEvaluator(data) )
 {
-	// test eigen levenberg marquardt
-	Eigen::VectorXd x(1);
-	x(0) = 500.0;
-	std::cout << "x: " << x << std::endl;
-
-	my_functor functor;
-	Eigen::NumericalDiff<my_functor> numDiff(functor);
-	Eigen::LevenbergMarquardt<Eigen::NumericalDiff<my_functor>, double> lm(numDiff);
-	lm.parameters.maxfev = 2000;
-	lm.parameters.xtol = 1.0e-10;
-	//std::cout << lm.parameters.maxfev << std::endl;
-
-	int ret = lm.minimize(x);
-	std::cout << lm.iter << std::endl;
-	std::cout << ret << std::endl;
-
-	std::cout << "x that minimizes the function: " << x << std::endl;
 }
 
 Eigen::VectorXd TLSGaussNewtonSolver::solve()
@@ -105,6 +48,7 @@ Eigen::VectorXd TLSGaussNewtonSolver::solve()
 	}
 	return parameterIterate;
 }
+
 
 Eigen::VectorXd TLSGaussNewtonSolver::getGNDirection(Eigen::VectorXd parameter)
 {
