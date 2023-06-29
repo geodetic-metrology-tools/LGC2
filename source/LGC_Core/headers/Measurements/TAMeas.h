7d5e6aa36d58c0662c5a5241e31682149ad46a1c
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2023-06-29 17:13:49 +0200

    use TStatusObject for activation and deactivation of measurements

--- source/LGC_Core/headers/Measurements/TAMeas.h
+++ source/LGC_Core/headers/Measurements/TAMeas.h
@@ -60,9 +60,6 @@ class TAMeas : public TStatusObject
 
 		/// Observation identifier: identification of specific measurement
 		std::string obsID;
-		
-		/// Mask status. Used for masking (ignoring measurements) during the adjustment while still evaluating all model data
-		bool maskStatus{false};
 
 		/// DB comment after the measurement definition
 		std::string eolcomment;
