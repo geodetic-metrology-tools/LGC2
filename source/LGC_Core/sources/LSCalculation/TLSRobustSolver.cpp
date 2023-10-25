bca79219818de5625672e7d8b8856d82dc8b05f8
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2023-10-25 15:03:53 +0200

    R/S formulation

--- source/LGC_Core/sources/LSCalculation/TLSRobustSolver.cpp
+++ source/LGC_Core/sources/LSCalculation/TLSRobustSolver.cpp
@@ -51,15 +51,14 @@ Eigen::VectorXd TLSRobustSolver::computeStep(Eigen::VectorXd par)
 
 
 	// assemble QP data:
-	// variables = y=(dx,V,W,U)
+	// variables = y=(dx,V,W,R,S)
 	// min 0.5*y^THy + g^T y  st.
 	// 0<=F(x_k)+Adx+BV<=0
-	// 0<=Sv V-W+U<=+inf			
-	// -inf<=V-W-U<=0
-	// 0<=C(x_k)+C_kdx<=0
+	// 0<=Sv V-W-R+S<=0			
+	// 0<=R,S
 	// 
-	// with H=diag(0,0,I,0), Sv=sqrt(Pv)
-	// g=lambda*(0,0,1^T,0)
+	// with H=diag(0,0,I,0,0), Sv=sqrt(Pv)
+	// g=lambda*(0,0,0,I,I)
 
 	UEOIndices indices = fEvaluator->dimensions;
 
@@ -68,8 +67,8 @@ Eigen::VectorXd TLSRobustSolver::computeStep(Eigen::VectorXd par)
 	int nEqn = indices.EIndex;
 	int nCnstr = indices.CIndex;
 
-	int nVariables = nPar + 3 * nObs;
-	int nConstraints = nEqn + 2 * nObs + nCnstr;
+	int nVariables = nPar + 4 * nObs;
+	int nConstraints = nEqn + nCnstr + 3 * nObs;
 
 	Eigen::SparseMatrix<double> constraintMatrix(nConstraints, nVariables);
 	Eigen::VectorXd lb(nConstraints), ub(nConstraints);
@@ -85,6 +84,9 @@ Eigen::VectorXd TLSRobustSolver::computeStep(Eigen::VectorXd par)
     OsqpEigen::Solver solver;
 	// no output printing
 //	solver.settings()->setVerbosity(false);
+	solver.settings()->setScaling(false);
+	solver.settings()->setScaledTerimination(true);
+	solver.settings()->setPolish(true);
 	solver.data()->setNumberOfVariables(nVariables);
 	solver.data()->setNumberOfConstraints(nConstraints);
 	if (!solver.data()->setHessianMatrix(hessian))
@@ -151,13 +153,14 @@ void TLSRobustSolver::setHuberConstraints(Eigen::VectorXd par, Eigen::SparseMatr
 
 
 	int nnzA(A.nonZeros()), nnzB(B.nonZeros()), nnzA2(A2.nonZeros()), nnzP(P.nonZeros());
-	int nnzHuber = nnzA + nnzA2 + nnzB + 2 * nnzP + 4 * nObs;
+	int nnzHuber = nnzA + nnzA2 + nnzB + 1 * nnzP + 5 * nObs;
 
 	// Constraints in matrix form
-	// -F(x_k) <= [A  , B, 0 ,0] [dx] <= -F(x_k)
-	// 0       <= [0  ,Sv,-I, I] [V ] <= +inf
-	// -inf    <= [0  ,Sv,-I,-I] [W ] <= 0
-	// -C(x_k) <= [C_k, 0, 0, 0] [U ] <= -C(x_k)
+	// -F(x_k) <= [A  , B, 0 ,0, 0] [dx] <= -F(x_k)
+	// -C(x_k) <= [C_k, 0, 0, 0, 0] [V ] <= -C(x_k)
+	// 0       <= [0  ,Sv,-I,-I, I] [W ] <= 0
+	// 0       <= [0  , 0, 0, I, 0] [R ] <= +inf
+	// 0       <= [0  , 0, 0, 0, I] [S ] <= +inf
 	std::vector<Eigen::Triplet<double>> triplets;
 	triplets.reserve(nnzHuber);
 	// row 1
@@ -174,40 +177,43 @@ void TLSRobustSolver::setHuberConstraints(Eigen::VectorXd par, Eigen::SparseMatr
 			triplets.push_back(TTriplet(it.row(), it.col() + nPar, it.value()));
 	}
 	// row 2
-	// Sv
-	for (int k = 0; k < Sv.outerSize(); k++)
+	// C_k
+	for (int k = 0; k < A2.outerSize(); ++k)
 	{
-		for (TSparseMatrix::InnerIterator it(Sv, k); it; ++it)
-			triplets.push_back(TTriplet(it.row() + nEqn, it.col() + nPar, it.value()));
-	}
-	// the two identity matrices
-	for (int k = 0; k < nObs; k++)
-	{
-		triplets.push_back(TTriplet(nEqn + k, nPar+nObs + k, -1.0));
-		triplets.push_back(TTriplet(nEqn + k, nPar+2 * nObs + k, +1.0));
+		for (TSparseMatrix::InnerIterator it(A2, k); it; ++it)
+			triplets.push_back(TTriplet(it.row() + nEqn, it.col(), it.value()));
 	}
 	// row 3
 	// Sv
 	for (int k = 0; k < Sv.outerSize(); k++)
 	{
 		for (TSparseMatrix::InnerIterator it(Sv, k); it; ++it)
-			triplets.push_back(TTriplet(it.row() + nEqn + nObs, it.col() + nPar, it.value()));
+			triplets.push_back(TTriplet(it.row() + nEqn+nCnstr, it.col() + nPar, it.value()));
 	}
-	// the two identity matrices
+	// the three identity matrices
 	for (int k = 0; k < nObs; k++)
 	{
-		triplets.push_back(TTriplet(nEqn + nObs + k,nPar+ nObs + k, -1.0));
-		triplets.push_back(TTriplet(nEqn + nObs + k, nPar+2 * nObs + k, -1.0));
+		triplets.push_back(TTriplet(nEqn + nCnstr + k, nPar + nObs + k, -1.0));
+		triplets.push_back(TTriplet(nEqn + nCnstr + k, nPar + 2 * nObs + k, -1.0));
+		triplets.push_back(TTriplet(nEqn + nCnstr + k, nPar + 3 * nObs + k, +1.0));
 	}
 	// row 4
-	// C_k
-	for (int k = 0; k < A2.outerSize(); ++k)
+	// one identity matrix
+	for (int k = 0; k < nObs; k++)
 	{
-		for (TSparseMatrix::InnerIterator it(A2, k); it; ++it)
-			triplets.push_back(TTriplet(it.row() + nEqn + 2 * nObs, it.col(), it.value()));
+		triplets.push_back(TTriplet(nEqn + nCnstr + nObs + k, nPar + 2 * nObs + k, +1.0));
+	}
+	// row 5
+	// one identity matrix
+	for (int k = 0; k < nObs; k++)
+	{
+		triplets.push_back(TTriplet(nEqn + nCnstr + 2 * nObs + k, nPar + 3 * nObs + k, +1.0));
 	}
 
+
 	constraintMat.setFromTriplets(triplets.begin(), triplets.end());
+	constraintMat.makeCompressed();
+	std::cout << "huber constraint matrix" << std::endl << constraintMat.toDense() << std::endl;
 
 	double BIG = 1e+6;
 	// the bounds
@@ -215,14 +221,17 @@ void TLSRobustSolver::setHuberConstraints(Eigen::VectorXd par, Eigen::SparseMatr
 	lb.topRows(nEqn) = -W;
 	ub.topRows(nEqn) = -W;
 	// row 2
-	lb.middleRows(nEqn, nObs).setZero();
-	ub.middleRows(nEqn, nObs).setConstant(OsqpEigen::INFTY);
+	lb.middleRows(nEqn, nCnstr) = -W2;
+	ub.middleRows(nEqn, nCnstr) = -W2;
 	// row 3
-	lb.middleRows(nEqn + nObs, nObs).setConstant(-OsqpEigen::INFTY);
-	ub.middleRows(nEqn + nObs, nObs).setZero();
+	lb.middleRows(nEqn + nCnstr, nObs).setZero();
+	ub.middleRows(nEqn + nCnstr, nObs).setZero();
 	// row 4
-	lb.middleRows(nEqn + 2 * nObs, nCnstr) = -W2;
-	ub.middleRows(nEqn + 2 * nObs, nCnstr) = -W2;
+	lb.middleRows(nEqn + nCnstr + nObs, nObs).setZero();
+	ub.middleRows(nEqn + nCnstr + nObs, nObs).setConstant(OsqpEigen::INFTY);
+	// row 5
+	lb.middleRows(nEqn + nCnstr + 2 * nObs, nObs).setZero();
+	ub.middleRows(nEqn + nCnstr + 2 * nObs, nObs).setConstant(OsqpEigen::INFTY);
 
 }
 
@@ -255,19 +264,20 @@ void TLSRobustSolver::setHuberObjective(Eigen::VectorXd par, Eigen::SparseMatrix
 	hessian.setFromTriplets(triplets.begin(), triplets.end());
 	hessian.makeCompressed();
 	Eigen::MatrixXd test = hessian.toDense();
+	std::cout << "huber Hessians matrix" << std::endl << test << std::endl;
 
 	// gradient
 	// the gradient is set to the value where the L2 switches to L1 in the Huber objective
 	// so it has roughly the magnitude of the sigmas
 	//Eigen::VectorXd diag = Sv.diagonal();
 	//Eigen::VectorXd sigmas = diag.cwiseInverse();
-	Eigen::VectorXd ones(nObs);
+	Eigen::VectorXd ones(2 * nObs);
 	ones.setOnes();
 		//	std::cout << sigmas << std::endl;
-	Eigen::VectorXd result(nPar + 3 * nObs);
+	Eigen::VectorXd result(nPar + 4 * nObs);
 	result.setZero();
 	//double gamma = 1;
-	result.middleRows(nPar + 2*nObs, nObs) = gamma*ones;
+	result.middleRows(nPar + 2 * nObs, 2 * nObs) = gamma * ones;
 
 
 	gradient = result;
