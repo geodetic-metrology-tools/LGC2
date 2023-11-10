90583b1c432f8e2d72da283bb35d0f68a9fe0b18
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2023-11-10 17:39:43 +0100

    simple huber solver using clarabel qp solver

---
+++ python/analyzer.py
@@ -0,0 +1,186 @@
+import clarabel
+import pyLGC
+import mplcursors
+import copy
+import numpy as np
+import scipy.sparse as sp
+from dataclasses import dataclass
+import seaborn as sns
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+def cast2HuberQP(P,A,A2,B,W,W2,huberGamma):
+    ## taking the design matrices and the weight  matrix.
+    ## huber gamma defines from which multiple of the standard deviation an residual is penalized linearly
+    ## setting up the Huber QP
+    ## returning Hessian and linear equality constraints+inequality constraints
+
+    # variables are
+    # dx,V,W,R,S
+    nPar=A.shape[1]
+    nEq=A.shape[0]
+    nCnstr=A2.shape[0]
+    nObs=P.shape[0]
+    nHuberPar = nPar+4*nObs
+    nHuberEq= nEq+nCnstr+nObs
+    nHuberIneq=2*nObs
+    ZeroObsxObs =sp.csr_matrix((nObs,nObs))
+    Iobs=sp.eye(nObs)
+    diagonalElements=P
+    roots = np.sqrt(diagonalElements)
+    Sv=sp.diags(roots)
+    A2fill=sp.csr_matrix((nCnstr,4*nObs))
+    # equality constraint matrix 
+    Ceq=sp.bmat([[A,B,ZeroObsxObs,ZeroObsxObs,ZeroObsxObs],
+                 [A2,None,None,None,None],
+                 [sp.csr_matrix((nObs,nPar)),Sv,-Iobs,-Iobs,Iobs]])
+    ##Ceq=sp.bmat([[A,B,ZeroObsxObs,ZeroObsxObs,ZeroObsxObs],
+    ##               [A2,sp.csr_matrix((nCnstr,4*nObs))],
+    ##               [sp.csr_matrix((nObs,nPar)),Sv,-Iobs,-Iobs,Iobs]])
+    # inequality constraint matrix 
+    Cineq=sp.bmat([[sp.csr_matrix((nObs,nPar+2*nObs)),Iobs,sp.csr_matrix((nObs,nObs))],
+                   [sp.csr_matrix((nObs,nPar+2*nObs)),sp.csr_matrix((nObs,nObs)),Iobs]])
+    # equality bound
+    bEq=np.concatenate((-W,-W2,np.zeros((nObs))))
+    # inequality bounds
+    BIG =1e+6
+    lbIneq=np.zeros(2*nObs)
+    ubIneq=BIG*np.ones(2*nObs)
+    # hessian 
+    hessDiag=np.concatenate((np.zeros(nPar+nObs),np.ones(nObs),np.zeros(2*nObs)))
+    hessian=sp.diags(hessDiag)
+    # gradient
+    gradient=huberGamma*np.concatenate((np.zeros(nPar+2*nObs),np.ones(2*nObs)))
+    return [hessian,gradient,Ceq,Cineq,bEq,lbIneq,ubIneq]
+
+def solveQPwithClarabel(hessian, gradient, Ceq, Cineq, bEq, lbIneq, ubIneq):
+    ## first tests: this seems to be fast
+    nParQP = gradient.shape[0]
+    nEqQP = Ceq.shape[0]
+    nIneqQP = Cineq.shape[0]
+    #nHuberPar = nPar+4*nObs
+    #nHuberEq= nEq+nCnstr+nObs
+    #nHuberIneq=2*nObs
+    AClarabel = sp.bmat([[Ceq], [Cineq], [-Cineq]])
+    bClarabel = np.concatenate((bEq, ubIneq, -lbIneq))
+    cones = [clarabel.ZeroConeT(nEqQP), clarabel.NonnegativeConeT(2*nIneqQP)]
+
+    settings = clarabel.DefaultSettings()
+    #settings.verbose = True
+    settings.verbose = False
+    sparseHessian=hessian.tocsc()
+    solver = clarabel.DefaultSolver(
+        sparseHessian, gradient, AClarabel.tocsc(), bClarabel, cones, settings)
+    solution = solver.solve()
+    # test solution equality constraint
+    # print("solution quality",np.linalg.norm(Ceq@solution.x-bEq))
+    return np.array(solution.x)
+
+@dataclass
+class huberSolution:
+    # primal solution of the estimation problem
+    primalSolution: np.array
+    # v=residual
+    residual:np.array
+    # w=quadratic weighted part
+    quadraticPart: np.array
+    # r,s the linear parts: r the positive part, s negative, one of them should be enforced zero by the huber solver
+    # so together : sqrt(Pv)v-w=r-s
+    posLinearPart:np.array
+    negLinearPart:np.array
+    fullSol:np.array
+    # including the weight matrix at the solution. it contains useful information for the analysis of the solution
+    weightDiagonal:np.array
+    # the huber gamma of the problem
+    huberGamma: float
+
+    def showQQPlot(self):
+        # generate a theoretical quantile vs real quantile plot
+        fig,ax=plt.subplots()
+        yData= self.weightDiagonal*self.residual
+        sm.qqplot(yData, line ='45',ax=ax) # 
+        plt.title("Q-Q Plot of Residuals. Huber Gamma="+str(self.huberGamma))
+        linPart=self.negLinearPart+self.posLinearPart
+        numberLinearContributions=np.sum(np.abs(linPart)>1e-4)
+        plt.xlabel("Theoretical Quantiles."+str(numberLinearContributions)+" Residuals above Huber threshold.")
+        plt.ylabel("Sample Quantiles")
+
+        # Draw a horizontal line at the specified height
+        plt.axhline(y=self.huberGamma, color='green', linestyle='--',label="Huber Threshold "+str(self.huberGamma))
+        plt.axhline(y=-self.huberGamma, color='green', linestyle='--')
+
+        # Extract the line data (theoretical and actual quantiles)
+        line = ax.get_lines()[0]
+
+        plt.legend()
+
+        # # Get the data points from the plot
+        # qq_line = ax.get_lines()[0]
+        # x = qq_line.get_xdata()
+        # y = qq_line.get_ydata()
+
+        # # Color points above the threshold in red
+        # above_threshold = self.residual > self.huberGamma
+        # ax.scatter(x[above_threshold], y[above_threshold], color='red',s=100)
+        # #ax.scatter(x[10], y[10], color='red',s=100)
+
+        # Show the plot
+        plt.show(block=False)
+
+                # Add interactive cursor
+        cursor = mplcursors.cursor(ax, hover=True)
+        @cursor.connect("add")
+        def on_add(sel):
+             # Find the index of the point closest to the cursor
+            distance = np.sqrt((yData - sel.target[1])**2)
+            index = np.argmin(distance)
+            sel.annotation.set(text=f"ObservationIndex: {index}")
+
+
+
+
+class huberSolver:
+    def __init__(self, evaluator):
+        self._evaluator = evaluator
+
+    def solve(self,initialValue,gamma,maxIter=50):
+        #solving the huber estimation problem with threshold huberGamma
+        evaluator = self._evaluator
+        par=copy.deepcopy(initialValue)
+        it=0
+        dx=1
+        while (it<maxIter)& (np.linalg.norm(dx)>1e-4):
+            it+=1
+            A=evaluator.getFirstDesignMatrix(par)
+            A2=evaluator.getFirstConstraintDesignMatrix(par)
+            B=evaluator.getSecondDesignMatrix(par)
+            Pv=evaluator.getPv(par)
+            #print(Pv)
+            W=evaluator.getMisclosure(par)
+            W2=evaluator.getConstraintMisclosure(par) 
+            [hessian, gradient, Ceq, Cineq, bEq, lbIneq,
+                ubIneq] = cast2HuberQP(Pv, A, A2, B, W, W2,gamma)
+            xClara = solveQPwithClarabel(hessian, gradient, Ceq, Cineq, bEq, lbIneq, ubIneq)
+            dx = xClara[:A.shape[1]]
+
+            par+=dx
+            ##print(rs)
+            print("|dx|=",np.linalg.norm(dx))
+            ##print("x=",par)
+            ##print("x+dx=",par) 
+        
+        # extract the huber specific parts
+        nPar=A.shape[1]
+        nObs=A.shape[0]
+        nCnstr=A2.shape[0]
+        # residual
+        v=xClara[nPar:nPar+nObs]
+        # quadratic part
+        w=xClara[nPar+nObs:nPar+2*nObs]
+        # linear parts
+        r=xClara[nPar+2*nObs:nPar+3*nObs]
+        s=xClara[nPar+3*nObs:nPar+4*nObs]
+        fullSol=xClara
+
+        
+        return huberSolution(par,v,w,r,s,fullSol, np.sqrt(Pv),gamma)
---
+++ python/usingHuberSolver.py
@@ -0,0 +1,121 @@
+## simple implementation of a huber estimator using pyLGC and proxsuite
+
+import seaborn as sns
+import statsmodels.api as sm
+import proxsuite
+from quadprog import solve_qp
+import clarabel
+import pyLGC
+import numpy as np
+import scipy.sparse as sp
+from scipy import sparse
+from scipy.io import mmread
+import matplotlib.pyplot as plt
+from analyzer import huberSolver,huberSolution
+
+#obj = pyLGC.LGCController("robust.lgc")
+#obj = pyLGC.LGCController("inclTest.lgc")
+#obj = pyLGC.LGCController("SUS-1790_23826_reseau_VV.lgc")
+#obj = pyLGC.LGCController("PSR2AD-NTOF_CALA-PSR-AD_noPLR3D.lgc")
+#obj = pyLGC.LGCController("12492_lgcv2_brut.lgc")
+#obj = pyLGC.LGCController("AR_LIBRE.lgc")
+#obj = pyLGC.LGCController("simple.lgc")
+#obj = pyLGC.LGCController("tstnHuber_NoBlunder.lgc")
+#obj = pyLGC.LGCController("tstnHuber_NoBlunder.sim")
+#obj = pyLGC.LGCController("LHC_LGC2_D2M-Q2S_OLOC.sim")
+#obj = pyLGC.LGCController("LHC_LGC2_D2M-Q2S_OLOC.inp")
+#obj = pyLGC.LGCController("15925-v5.lgc")
+#obj = pyLGC.LGCController("LHC_2013-Compense_v2-LGC_1.12.02.lgc")
+#obj = pyLGC.LGCController("LHC_2013-Compense_v2-LGC_1.12.02.sim")
+#obj = pyLGC.LGCController("huberExamples/PSR2AD-NTOF_CALA-PSR-AD.lgc")
+obj = pyLGC.LGCController("huberExamples/28360.lgc")
+#obj = pyLGC.LGCController("PSR2AD-NTOF_CALA-PSR-AD.sim")
+#obj = pyLGC.LGCController("huberExamples/15217.lgc")
+#obj = pyLGC.LGCController("huberExamples/simpleMinimal.lgc")
+#obj = pyLGC.LGCController("huberExamples/SUS-1910_ScanLGC_frames_OBSXYZ_OK_BF.lgc")
+
+myTool= huberSolver(obj)
+initialValue = obj.getParameter()
+#for huberGamma in [10,5,2,1,0.0001]:
+#for huberGamma in [1.5]:
+for huberGamma in [2.95,100000]:
+    print("Attempt Huber problem with gamma =",huberGamma)
+    solution= myTool.solve(initialValue, huberGamma,maxIter=10)
+    solution.showQQPlot()
+    diffNorm=np.linalg.norm(solution.primalSolution-initialValue)
+    linPart=solution.negLinearPart+solution.posLinearPart
+    #print("linear part=",np.linalg.norm(linPart))
+    #print("solution=",solution.primalSolution)
+    # print("V,W,R,S")
+    # print(solution.residual[646],solution.quadraticPart[646],solution.posLinearPart[646],solution.negLinearPart[646])
+    numberLinearContributions=np.sum(np.abs(linPart)>1e-4)
+    
+    print("number of linear contributions = ",numberLinearContributions)
+    print("difference to previous huber solution=",diffNorm)
+    initialValue=solution.primalSolution
+
+
+    
+#rs = x[-2*nObs:]
+#print(rs)
+#print("number of potential outliers=", np.count_nonzero(np.abs(rs)>2))
+#indices=np.where(np.abs(rs)>0.1)
+#print(indices,rs[indices])
+#print("solution=",x[:nPar])
+
+#solution.showQQPlot()
+
+#P=obj.getPv(solution.primalSolution)
+#weight=np.sqrt(P)
+### weighted residuals
+#residuals=weight*solution.residual
+#absResiduals=np.abs(residuals)
+#max_value = absResiduals.max()
+#max_index = np.argmax(absResiduals)
+#n = 5
+#indices = np.argpartition(absResiduals, -n)[-n:]
+#sortedIndices = indices[np.argsort(-absResiduals[indices])]
+#
+#linParts = solution.negLinearPart+solution.posLinearPart
+#linIndices = np.argpartition(linParts, -n)[-n:]
+#sortedLinIndices=linIndices[np.argsort(-linParts[linIndices])]
+#print("biggest linear parts=",linParts[sortedLinIndices])
+#print("indices of biggest linear parts=",sortedLinIndices)
+#
+#
+#print(n," biggest abs residuals",absResiduals[sortedIndices])
+#print(" linearParts",solution.negLinearPart[sortedIndices]+solution.posLinearPart[sortedIndices])
+#
+#print("at obs-indices ",sortedIndices)
+#
+#
+#
+#print("Maximum value:", max_value, " at index ",max_index)
+#sm.qqplot(residuals, line ='45') # 
+#
+## Create the histogram with custom bins
+##plt.xscale('symlog')
+#
+## Show the plot
+#plt.show()
+
+## 
+## obsIndex=2
+## v=solution.residual
+## w=solution.quadraticPart
+## r=solution.posLinearPart
+## s=solution.negLinearPart
+## P=obj.getPv(solution.primalSolution)
+## weight=np.sqrt(P)
+## #print("Sv * v -w -r +s \n")
+## #print(weight,"*",v,"-",w,"-",r,"+",s,"\n")
+## #print(w-r+s)
+## #print(weight@v-w-r+s)
+## #print(solution.fullSol)
+## #
+
+
+# to prevent closing the windows immeadiatelly
+plt.show()
+
+True
