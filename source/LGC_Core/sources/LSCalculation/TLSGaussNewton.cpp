92b3b689afb8410fadafdb5e0e2ca7fb5a96fb2d
Jurgen Gutekunst <juergen.gutekunst@cern.ch>
2025-03-25 15:43:01 +0100

    changes in lm alg

--- source/LGC_Core/sources/LSCalculation/TLSGaussNewton.cpp
+++ source/LGC_Core/sources/LSCalculation/TLSGaussNewton.cpp
@@ -47,9 +47,11 @@ TVector TLSGaussNewton::solve(Eigen::VectorXd initial)
 //	dxTrial.setConstant(1);
 	
 	double lmPenalty = 1e-4;
+	//double lmPenalty = 0;
 	
 	// outer loop
-	while (dx.norm() > 1e-6)
+	int outerIt = 0;
+	while (dx.norm() > 1e-6 && (currentObjective>1e-16))
 	{
 		// prepare the ingredients that are needed for each inner loop step:
 		// the normal matrix (before regularization), the right hand side of the normal equation system
@@ -57,16 +59,20 @@ TVector TLSGaussNewton::solve(Eigen::VectorXd initial)
 		TSparseMatrix &N2_current = getN2Matrix(A_current, invN1_current);
 		TVector rhs_current(indices.UIndex + indices.CIndex);
 		rhs_current << A_current.transpose()*invN1_current*W_current, W2_current;
+		outerIt++;
+		std::cout << "outer iteration " << outerIt << " current objective " << currentObjective << std::endl;
 		//rhs_current.topRows(indices.UIndex)=W_current		
 		// inner loop
 		bool acceptLMStep = false;
-
+		int innerIt = 0;
 		while (!acceptLMStep)
 		{
+			innerIt++;
+			std::cout << "		inner iteration " << innerIt;
 			dxTrial = computeRegularizedStep(N2_current, C_current, rhs_current, lmPenalty);
 			trialIterate = currentIterate + dxTrial;
 
-			std::cout << "proposed |dx_inner|=" << dxTrial.norm() << std::endl;
+			std::cout << " with LM factor "<< lmPenalty << " => proposed |dx_inner|=" << dxTrial.norm();
 
 			// compute predicted objective
 			predictedObjective = computeObjective(A_current, invB_current, Pv_current, W_current, dxTrial);
@@ -84,9 +90,11 @@ TVector TLSGaussNewton::solve(Eigen::VectorXd initial)
 			// TVector &W2_trial= fEvaluator->getConstraintMisclosure();
 			trialObjective = computeObjective(A_trial, invB_trial, Pv_trial, W_trial);
 			acceptLMStep = decideAcceptance(currentObjective, predictedObjective, trialObjective);
+			std::cout << " . Step accepted: " << acceptLMStep << std::endl;
+			
 			if (acceptLMStep){
 				// decrease lm penalty
-				lmPenalty /= 3;
+				lmPenalty /= 10;
 				// update data needed for the next outer iteration
 				invB_current = invB_trial;
 				Pv_current = Pv_trial;
@@ -107,7 +115,7 @@ TVector TLSGaussNewton::solve(Eigen::VectorXd initial)
 
 
 
-
+	result = currentIterate;
 	return result;
 }
 
@@ -133,7 +141,9 @@ TVector TLSGaussNewton::computeRegularizedStep(TSparseMatrix &N2, TSparseMatrix 
 	// assemble the regularized normal matrix with constraints
 	TSparseMatrix NBigRegularized = getNBig(N2, C, lmPenalty);
 	TVector solutionExt(indices.UIndex + indices.CIndex);
+	solutionExt.setZero();
 	bool solveSuccess = TSparseUtils::solveUnique(NBigRegularized, -rhs, solutionExt, (indices.CIndex == 0), (indices.CIndex == 0));
+//	std::cout << "lin system sol" << std::endl << solutionExt << std::endl;
 	if (!solveSuccess)
 	{
 		throw std::runtime_error("problem during step computation.");
@@ -147,16 +157,18 @@ bool TLSGaussNewton::decideAcceptance(double currObj, double predObj, double tri
 	bool accept = true;
 	double predictedDecrease = currObj - predObj;
 	double actualDecrease = currObj - trialObj;
-	std::cout << "cObj=" << currObj << std::endl;
-	std::cout << "pObj=" << predObj << std::endl;
-	std::cout << "tObj=" << trialObj << std::endl;
-	std::cout << "pRed=" << predictedDecrease << std::endl;
-	std::cout << "aRed=" << actualDecrease << std::endl;
+//	std::cout << "cObj=" << currObj << std::endl;
+//	std::cout << "pObj=" << predObj << std::endl;
+//	std::cout << "tObj=" << trialObj << std::endl;
+//	std::cout << "pRed=" << predictedDecrease << std::endl;
+//	std::cout << "aRed=" << actualDecrease << std::endl;
+	if (trialObj < 1e-16)
+		return true;
 	if (actualDecrease < 0)
 		accept &= false;
 	if (predictedDecrease < 0)
 		throw std::runtime_error("predicted objective increased! We need decrease");
-	double acceptanceFactor = 0.2;
+	double acceptanceFactor = 0.01;
 	if (actualDecrease > acceptanceFactor * predictedDecrease)
 		accept &= true;
 	return accept;
@@ -178,20 +190,22 @@ TSparseMatrix TLSGaussNewton::getN2Matrix(TSparseMatrix &A, TSparseMatrix &invN1
 TSparseMatrix TLSGaussNewton::getNBig(TSparseMatrix &N2, TSparseMatrix &C, double lmCoefficient)
 {
 	TSparseMatrix N2regularized = N2;
+	//std::cout << "N2=" <<std::endl<< N2.toDense() << std::endl;
 	// apply the LM regularization. Add lmCoefficient * diagonal entry to the diagonal (this corresponds to a variable scaling)
 	for (int j = 0; j < indices.UIndex; j++)
 	{
 		N2regularized.coeffRef(j, j) *= lmCoefficient + 1;
 	}
 
+	//std::cout << "N2reg=" << std::endl << N2regularized.toDense() << std::endl;
 	TSparseMatrix NBig(indices.UIndex + indices.CIndex, indices.UIndex + indices.CIndex);
 	std::vector<TTriplet> coeffs;
-	coeffs.reserve(N2.nonZeros() + 2 * C.nonZeros());
+	coeffs.reserve(N2regularized.nonZeros() + 2 * C.nonZeros());
 
-	// Fill in the N2 part
-	for (int k = 0; k < N2.outerSize(); ++k)
+	// Fill in the N2regularized part
+	for (int k = 0; k < N2regularized.outerSize(); ++k)
 	{
-		for (TSparseMatrix::InnerIterator it(N2, k); it; ++it)
+		for (TSparseMatrix::InnerIterator it(N2regularized, k); it; ++it)
 			coeffs.push_back(TTriplet(it.row(), it.col(), it.value()));
 	}
 
@@ -200,8 +214,8 @@ TSparseMatrix TLSGaussNewton::getNBig(TSparseMatrix &N2, TSparseMatrix &C, doubl
 	{
 		for (TSparseMatrix::InnerIterator it(C, k); it; ++it)
 		{
-			coeffs.push_back(TTriplet(it.row() + N2.rows(), it.col(), it.value())); // C
-			coeffs.push_back(TTriplet(it.col(), it.row() + N2.cols(), it.value())); // CT
+			coeffs.push_back(TTriplet(it.row() + N2regularized.rows(), it.col(), it.value())); // C
+			coeffs.push_back(TTriplet(it.col(), it.row() + N2regularized.cols(), it.value())); // CT
 		}
 	}
 	NBig.setFromTriplets(coeffs.begin(), coeffs.end());
