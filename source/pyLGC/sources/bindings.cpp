9f4404a4343e0272ae7139044d49a1e51573b8ad
Marco Carotta <marco.carotta@cern.ch>
2025-03-28 16:39:42 +0100

    minor changes

--- source/pyLGC/sources/bindings.cpp
+++ source/pyLGC/sources/bindings.cpp
@@ -15,6 +15,13 @@ PYBIND11_MODULE(pyLGC, m)
         .def(py::init<std::shared_ptr<TLGCData>>(), py::arg("data"))
         .def(py::init<std::stringstream &>(), py::arg("fileStream"))
 
+        // Custom constructor binding that accepts a Python string (str) and converts it to std::stringstream
+        .def(py::init([](const std::string& content) {
+            std::stringstream stream(content);  // Create a stringstream from the Python string
+            return new TLSEvaluator(stream);    // Return a new instance of TLSEvaluator
+        }), py::arg("content"))  // Bind the constructor to a string input
+		
+
         // Get and set parameters
         .def("setParameters", &TLSEvaluator::setParameters, py::arg("para"))
         .def("getEstParams", &TLSEvaluator::getEstParams)
@@ -26,29 +33,29 @@ PYBIND11_MODULE(pyLGC, m)
 
         // Evaluation and matrices
         .def("evaluate", &TLSEvaluator::evaluate)
-        .def("getMisclosure", &TLSEvaluator::getMisclosure)
+		.def("getMisclosure", &TLSEvaluator::getMisclosure, py::return_value_policy::reference) // for the silent error adding py::return_value_policy::reference should work
         .def("getAMatrix", &TLSEvaluator::getAMatrix)
         .def("getBMatrix", &TLSEvaluator::getBMatrix)
         .def("getConstraintMisclosure", &TLSEvaluator::getConstraintMisclosure)
         .def("getA2Matrix", &TLSEvaluator::getA2Matrix)
         .def("getPMatrix", &TLSEvaluator::getPMatrix)
 
-        // Additional setter and getter helpers
-        .def("setPointParams", &TLSEvaluator::setPointParams, py::arg("para"))
-        .def("setAngleParams", &TLSEvaluator::setAngleParams, py::arg("para"))
-        .def("setPlaneParams", &TLSEvaluator::setPlaneParams, py::arg("para"))
-        .def("setLengthParams", &TLSEvaluator::setLengthParams, py::arg("para"))
-        .def("setTransformationParams", &TLSEvaluator::setTransformationParams, py::arg("para"))
-        .def("setLineParams", &TLSEvaluator::setLineParams, py::arg("para"))
+        //// Additional setter and getter helpers
+        //.def("setPointParams", &TLSEvaluator::setPointParams, py::arg("para"))
+        //.def("setAngleParams", &TLSEvaluator::setAngleParams, py::arg("para"))
+        //.def("setPlaneParams", &TLSEvaluator::setPlaneParams, py::arg("para"))
+        //.def("setLengthParams", &TLSEvaluator::setLengthParams, py::arg("para"))
+        //.def("setTransformationParams", &TLSEvaluator::setTransformationParams, py::arg("para"))
+        //.def("setLineParams", &TLSEvaluator::setLineParams, py::arg("para"))
 
-        .def("getPointParams", &TLSEvaluator::getPointParams)
-        .def("getAngleParams", &TLSEvaluator::getAngleParams)
-        .def("getPlaneParams", &TLSEvaluator::getPlaneParams)
-        .def("getLengthParams", &TLSEvaluator::getLengthParams)
-        .def("getTransformationParams", &TLSEvaluator::getTransformationParams)
-        .def("getLineParams", &TLSEvaluator::getLineParams)
-        
-        // Add any additional helper functions you might need, if required
-        .def("isUptoDate", &TLSEvaluator::isUptoDate)  // Example additional method
+        //.def("getPointParams", &TLSEvaluator::getPointParams)
+        //.def("getAngleParams", &TLSEvaluator::getAngleParams)
+        //.def("getPlaneParams", &TLSEvaluator::getPlaneParams)
+        //.def("getLengthParams", &TLSEvaluator::getLengthParams)
+        //.def("getTransformationParams", &TLSEvaluator::getTransformationParams)
+        //.def("getLineParams", &TLSEvaluator::getLineParams)
+        //
+        //// Add any additional helper functions you might need, if required
+        //.def("isUptoDate", &TLSEvaluator::isUptoDate)  // Example additional method
     ;
 }
