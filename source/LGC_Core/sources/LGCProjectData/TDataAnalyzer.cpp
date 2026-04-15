856cca9c05141e9795ace860a2dabdd2eb1753f3
gukautzm <guillaume.kautzmann@cern.ch>
2026-04-15 11:19:56 +0200

    fix: include point-sigma obs in line-number map
    Signed-off-by: gukautzm <guillaume.kautzmann@cern.ch>

--- source/LGC_Core/sources/LGCProjectData/TDataAnalyzer.cpp
+++ source/LGC_Core/sources/LGCProjectData/TDataAnalyzer.cpp
@@ -1324,7 +1324,11 @@ void TDataAnalyzer::assignEOIndices()
 			int nObservations = ptSigma.fRelObsIdx.size();
 			int nConstraints = ptSigma.fRelCIdx.size();
 			if (nObservations > 0)
+			{
 				ptSigma.firstObsIdx = fData.fUEOIndices.OIndex;
+				for (int i = 0; i < nObservations; ++i)
+					fData.fObsIndexToLineNumber[ptSigma.firstObsIdx + i] = pt.line;
+			}
 			if (nConstraints > 0)
 				ptSigma.firstCIdx = fData.fUEOIndices.CIndex;
 			fData.fUEOIndices.OIndex += nObservations;
