16cddbcd3f397505910488833b6f009a5eb92629
Juergen Gutekunst <juergen.gutekunst@cern.ch>
2026-06-04 16:25:01 +0200

    pre-commit compliance
    Signed-off-by: Juergen Gutekunst <juergen.gutekunst@cern.ch>

--- source/LGC_Core/sources/Measurements/TTSTN.cpp
+++ source/LGC_Core/sources/Measurements/TTSTN.cpp
@@ -7,132 +7,167 @@
 int TTSTN::stnCounter_ = 0;
 int TTSTN::TROM::romCounter_ = 0;
 
-void TTSTN::TROM::initialiseObsSummaries() {
-    // First clear the old contents away
-    plr3dSummary_.distObsSum.clear();
-    plr3dSummary_.anglObsSum.clear();
-    plr3dSummary_.zendObsSum.clear();
-    anglSummary_.clear();
-    zendSummary_.clear();
-    distSummary_.clear();
-    dhorSummary_.clear();
-    ecthSummary_.clear();
-    ecdirSummary_.clear();
+void TTSTN::TROM::initialiseObsSummaries()
+{
+	// First clear the old contents away
+	plr3dSummary_.distObsSum.clear();
+	plr3dSummary_.anglObsSum.clear();
+	plr3dSummary_.zendObsSum.clear();
+	anglSummary_.clear();
+	zendSummary_.clear();
+	distSummary_.clear();
+	dhorSummary_.clear();
+	ecthSummary_.clear();
+	ecdirSummary_.clear();
 
-    // Add the residuals of each measurement and initialise the obs summaries:
-    
-    if(measANGL.size() != 0) {
-        for(auto& ItANGL : measANGL)
-            anglSummary_.addNewResidual(ItANGL.getAngleResidual().getSignedCCValue());
+	// Add the residuals of each measurement and initialise the obs summaries:
 
-        anglSummary_.initialise();
-    }
+	if (measANGL.size() != 0)
+	{
+		for (auto &ItANGL : measANGL)
+			anglSummary_.addNewResidual(ItANGL.getAngleResidual().getSignedCCValue());
 
-    if(measZEND.size() != 0) {
-        for(auto& ItZEND : measZEND)
-            zendSummary_.addNewResidual(ItZEND.getAngleResidual().getSignedCCValue());
-    
-        zendSummary_.initialise();
-    }
+		anglSummary_.initialise();
+	}
 
-    if(measDIST.size() != 0){
-        for(auto& ItDIST : measDIST)
-            distSummary_.addNewResidual(ItDIST.getDistanceResidual().getMMetresValue());
+	if (measZEND.size() != 0)
+	{
+		for (auto &ItZEND : measZEND)
+			zendSummary_.addNewResidual(ItZEND.getAngleResidual().getSignedCCValue());
 
-        distSummary_.initialise();
-    }
+		zendSummary_.initialise();
+	}
 
-    if(measDHOR.size() != 0) {
-        for(auto& ItDHOR : measDHOR)
-            dhorSummary_.addNewResidual(ItDHOR.getDistanceResidual().getMMetresValue());
+	if (measDIST.size() != 0)
+	{
+		for (auto &ItDIST : measDIST)
+			distSummary_.addNewResidual(ItDIST.getDistanceResidual().getMMetresValue());
 
-        dhorSummary_.initialise();
-    }
-    
-    if(measECTH.size() != 0) {
-        for(auto& ItECTH : measECTH)
-            ecthSummary_.addNewResidual(ItECTH.getDistanceResidual().getMMetresValue());
+		distSummary_.initialise();
+	}
 
-        ecthSummary_.initialise();
-    }
+	if (measDHOR.size() != 0)
+	{
+		for (auto &ItDHOR : measDHOR)
+			dhorSummary_.addNewResidual(ItDHOR.getDistanceResidual().getMMetresValue());
 
-    if(measECDIR.size() != 0){
-        for(auto& ItECDIR : measECDIR)
-            ecdirSummary_.addNewResidual(ItECDIR.getDistanceResidual().getMMetresValue());
+		dhorSummary_.initialise();
+	}
 
-        ecdirSummary_.initialise();
-    }
+	if (measECTH.size() != 0)
+	{
+		for (auto &ItECTH : measECTH)
+			ecthSummary_.addNewResidual(ItECTH.getDistanceResidual().getMMetresValue());
 
-    if(measPLR3D.size() != 0){
-        for(auto& ItPLR3D : measPLR3D){
-            plr3dSummary_.distObsSum.addNewResidual(ItPLR3D.getDistanceResidual().getMMetresValue());
-            plr3dSummary_.anglObsSum.addNewResidual(ItPLR3D.getAngleResidual(EPLR3DAngles::kANGL).getSignedCCValue());
-            plr3dSummary_.zendObsSum.addNewResidual(ItPLR3D.getAngleResidual(EPLR3DAngles::kZEND).getSignedCCValue());
-        }
+		ecthSummary_.initialise();
+	}
 
-        plr3dSummary_.distObsSum.initialise();
-        plr3dSummary_.anglObsSum.initialise();
-        plr3dSummary_.zendObsSum.initialise();
-    }
+	if (measECDIR.size() != 0)
+	{
+		for (auto &ItECDIR : measECDIR)
+			ecdirSummary_.addNewResidual(ItECDIR.getDistanceResidual().getMMetresValue());
+
+		ecdirSummary_.initialise();
+	}
+
+	if (measPLR3D.size() != 0)
+	{
+		for (auto &ItPLR3D : measPLR3D)
+		{
+			plr3dSummary_.distObsSum.addNewResidual(ItPLR3D.getDistanceResidual().getMMetresValue());
+			plr3dSummary_.anglObsSum.addNewResidual(ItPLR3D.getAngleResidual(EPLR3DAngles::kANGL).getSignedCCValue());
+			plr3dSummary_.zendObsSum.addNewResidual(ItPLR3D.getAngleResidual(EPLR3DAngles::kZEND).getSignedCCValue());
+		}
+
+		plr3dSummary_.distObsSum.initialise();
+		plr3dSummary_.anglObsSum.initialise();
+		plr3dSummary_.zendObsSum.initialise();
+	}
 }
 
 bool TTSTN::TROM::hasNonPLR3DMeasurements() const
 {
-	return !measANGL.empty() || !measZEND.empty() || !measDIST.empty()
-		|| !measDHOR.empty() || !measECTH.empty() || !measECDIR.empty();
+	return !measANGL.empty() || !measZEND.empty() || !measDIST.empty() || !measDHOR.empty() || !measECTH.empty() || !measECDIR.empty();
 }
 
-const TLGCObsSummary& TTSTN::TROM::getANGLObsSummary() const { return anglSummary_; }
+const TLGCObsSummary &TTSTN::TROM::getANGLObsSummary() const
+{
+	return anglSummary_;
+}
 
-const TLGCObsSummary& TTSTN::TROM::getANGLObsSummary(std::string text) noexcept {
+const TLGCObsSummary &TTSTN::TROM::getANGLObsSummary(std::string text) noexcept
+{
 	anglSummary_.setObsText(text);
 	return anglSummary_;
 }
 
-const TLGCObsSummary& TTSTN::TROM::getZENDObsSummary() const { return zendSummary_; }
+const TLGCObsSummary &TTSTN::TROM::getZENDObsSummary() const
+{
+	return zendSummary_;
+}
 
-const TLGCObsSummary& TTSTN::TROM::getZENDObsSummary(std::string text) noexcept {
+const TLGCObsSummary &TTSTN::TROM::getZENDObsSummary(std::string text) noexcept
+{
 	zendSummary_.setObsText(text);
 	return zendSummary_;
 }
 
-const TLGCObsSummary&  TTSTN::TROM::getDISTObsSummary() const { return distSummary_; }
+const TLGCObsSummary &TTSTN::TROM::getDISTObsSummary() const
+{
+	return distSummary_;
+}
 
-const TLGCObsSummary& TTSTN::TROM::getDISTObsSummary(std::string text) noexcept {
+const TLGCObsSummary &TTSTN::TROM::getDISTObsSummary(std::string text) noexcept
+{
 	distSummary_.setObsText(text);
 	return distSummary_;
 }
 
-const TLGCObsSummary&  TTSTN::TROM::getDHORObsSummary() const { return dhorSummary_; }
+const TLGCObsSummary &TTSTN::TROM::getDHORObsSummary() const
+{
+	return dhorSummary_;
+}
 
-const TLGCObsSummary& TTSTN::TROM::getDHORObsSummary(std::string text) noexcept {
+const TLGCObsSummary &TTSTN::TROM::getDHORObsSummary(std::string text) noexcept
+{
 	dhorSummary_.setObsText(text);
 	return dhorSummary_;
 }
 
-const TLGCObsSummary&  TTSTN::TROM::getECTHObsSummary() const { return ecthSummary_; }
+const TLGCObsSummary &TTSTN::TROM::getECTHObsSummary() const
+{
+	return ecthSummary_;
+}
 
-const TLGCObsSummary& TTSTN::TROM::getECTHObsSummary(std::string text) noexcept {
+const TLGCObsSummary &TTSTN::TROM::getECTHObsSummary(std::string text) noexcept
+{
 	ecthSummary_.setObsText(text);
 	return ecthSummary_;
 }
 
-const TLGCObsSummary&  TTSTN::TROM::getECDIRObsSummary() const { return ecdirSummary_; }
+const TLGCObsSummary &TTSTN::TROM::getECDIRObsSummary() const
+{
+	return ecdirSummary_;
+}
 
-const TLGCObsSummary& TTSTN::TROM::getECDIRObsSummary(std::string text) noexcept {
+const TLGCObsSummary &TTSTN::TROM::getECDIRObsSummary(std::string text) noexcept
+{
 	ecdirSummary_.setObsText(text);
 	return ecdirSummary_;
 }
 
-const TPOLARObsSummary& TTSTN::TROM::getPLR3DObsSummary() const { return plr3dSummary_; }
+const TPOLARObsSummary &TTSTN::TROM::getPLR3DObsSummary() const
+{
+	return plr3dSummary_;
+}
 
-const TPOLARObsSummary& TTSTN::TROM::getPLR3DObsSummary(std::string text) noexcept {
+const TPOLARObsSummary &TTSTN::TROM::getPLR3DObsSummary(std::string text) noexcept
+{
 	plr3dSummary_.anglObsSum.setObsText(text);
 	plr3dSummary_.zendObsSum.setObsText(text);
 	plr3dSummary_.distObsSum.setObsText(text);
 	return plr3dSummary_;
 }
-
 
 #if USE_SERIALIZER
 // Inherited via Serializable
@@ -199,4 +234,3 @@ void TTSTN::TROM::serialize(ObjectSerializer &obj) const
 		obj.addProperty("zendSummary_", zendSummary_);
 }
 #endif
-
