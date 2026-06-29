2b7fcca28d8219816f47c57ba7a7c58a88747aba
gukautzm <guillaume.kautzmann@cern.ch>
2026-06-29 15:42:23 +0200

    versionning update
    Signed-off-by: gukautzm <guillaume.kautzmann@cern.ch>

--- source/LGC_Core/LGCCore.cmake
+++ source/LGC_Core/LGCCore.cmake
@@ -2,7 +2,7 @@
 # /!\ IMPORTANT : Modify it int ../CMakeLists.txt AS WELL !
 set (LGC_VERSION_MAJOR "2")
 set (LGC_VERSION_MINOR "11")
-set (LGC_VERSION_PATCH "0")
+set (LGC_VERSION_PATCH "0_ENG_0")
 
 # Versions files
 configure_file (
